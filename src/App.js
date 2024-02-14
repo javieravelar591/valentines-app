628bd6ad5fd0b3c69399be37ff9bc104ab59ab5e
javieravelar591 <avela020@umn.edu>
2024-02-13 18:00:52 -0600

    more functionality

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import './App.css';
-import React from 'react';
+import './banana-cat.jpg';
+import catgif from './cat.gif';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  // const [showImage, setShowImage] = useState(false);
+  const [showText, setShowText] = useState(false);
+  const [showGif, setShowGif] = useState(false);
 
-  // const handleMouseOver = () => {
-  //   setShowImage(true);
-  // }
-  // const handleMouseOut = () => {
-  //   setShowImage(false);
-  // }
-  const button = document.getElementById('no-btn');
-  button.addEventListener('mouseover', function () {
-    console.log('moused over');
-    button.style.left = `${Math.ceil(Math.random() * 90)}%`;
-    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
-  });
-  button.addEventListener('click', function () {
-      alert('you clicked me')
-  })
+  useEffect(() => {
+    const button = document.getElementById('no-btn');
+    if (button) {
+      button.addEventListener('mouseover', function () {
+        console.log('moused over');
+        button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+        button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+      });
+      button.addEventListener('click', function () {
+          alert('you clicked me')
+      })
+    }
+  }, []);
+
+  const handleYesClick = () => {
+    setShowText(true);
+    setShowGif(true);
+  }
+
   return (
     <section className='app-section'>
-      <h1 className='app-header'>What's up stinky</h1>
-      <h1>Will you be my valentines :D</h1>
-      <button type='submit' value={'yes'} className='yes-btn'>Yes</button>
+      <h1 className='app-header'>What's up smelly</h1>
+      <div>
+        {showGif ? (
+          <img src={catgif} alt='catgif' />
+        ) : (
+          <img src='cat.jpg' alt='cat-image' />
+        )}
+      </div>
+      <h1>Will you be my valentines? (Try to press no :D)</h1>
+      <button type='submit' value={'yes'} className='yes-btn' onClick={handleYesClick}>Yes</button>
       <button type='submit' value={'no'} id='no-btn'>No</button>
-       {/* {showImage && (<img src='valentines-app/public/banana-cat.jpg' alt='banana-cat'/>)} */}
+       {showText && <p>WAHOO! I'm glad you made the right choice. Sorry we can't see eachother 
+        to celebrate and I can't afford to do anything big but soon we'll see eachother every day and
+        we'll be rich! :3</p>}
     </section>
   );
   
